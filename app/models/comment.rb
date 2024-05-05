48a8b4638e3f51d00700c439e24d501f38241547
Haewon Seok <114521565+haewonseok@users.noreply.github.com>
2024-05-05 22:35:28 +0000

    Completed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,13 +23,13 @@ class Comment < ApplicationRecord
   # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
   belongs_to(:photo, class_name: "Photo", foreign_key: "photo_id")
 
-  # def commenter
-  #   my_author_id = self.author_id
+  def commenter
+    my_author_id = self.author_id
 
-  #   matching_users = User.where({ :id => my_author_id })
+    matching_users = User.where({ :id => my_author_id })
 
-  #   the_user = matching_users.at(0)
+    the_user = matching_users.at(0)
 
-  #   return the_user
+    return the_user
   end
 end
